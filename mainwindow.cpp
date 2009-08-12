0990f6f91a625dfb8c186e8b1ce628a4b26bff41
Emily Dimpfl <emily.dimpfl@gmail.com>
2009-08-12 22:22:49 +0000

    View controls now adjust widget sizes nicely. Memory dump pane always opens at its largest width, as does the code tab.
    git-svn-id: http://pep8-1.googlecode.com/svn/trunk@326 0efa5a84-20a4-11de-9909-f37ff7dbbc12

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1068,6 +1068,11 @@ void MainWindow::on_actionView_Code_CPU_triggered()
     ui->actionView_Code_Only->setDisabled(false);
     ui->actionView_Code_CPU->setDisabled(true);
     ui->actionView_Code_CPU_Memory->setDisabled(false);
+    QList<int> list;
+    list.append(3000);
+    list.append(1);
+    list.append(1);
+    ui->horizontalSplitter->setSizes(list);
 }
 
 void MainWindow::on_actionView_Code_CPU_Memory_triggered()
@@ -1083,8 +1088,13 @@ void MainWindow::on_actionView_Code_CPU_Memory_triggered()
     ui->actionView_Code_Only->setDisabled(false);
     ui->actionView_Code_CPU->setDisabled(false);
     ui->actionView_Code_CPU_Memory->setDisabled(true);
-    memoryDumpPane->updateGeometry();
     memoryDumpPane->setMaximumWidth(memoryDumpPane->memoryDumpWidth());
+    QList<int> list;
+    list.append(3000);
+    list.append(1);
+    list.append(3000);
+    ui->horizontalSplitter->setSizes(list);
+
 }
 
 void MainWindow::on_actionView_Code_Tab_triggered()
